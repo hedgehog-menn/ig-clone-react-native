fd2b8b57fb298d90392f4288be1326bce39daac7
Kanokphan <kp.thananchat@gmail.com>
2022-08-28 19:10:23 +0700

    create dummy Stories

---
+++ data/users.js
@@ -0,0 +1,27 @@
+export const USERS = [
+  {
+    user: 'cleverqazi',
+    image:
+      'https://yt3.ggpht.com/ytc/AKedOLRY9Un_v7Xr9dG1F5NEkqGsGSqwqRz0O3w3r1mI=s900-c-k-c0x00ffffff-no-rj',
+  },
+  {
+    user: 'theqazman',
+    image:
+      'https://yt3.ggpht.com/ytc/AKedOLRY9Un_v7Xr9dG1F5NEkqGsGSqwqRz0O3w3r1mI=s900-c-k-c0x00ffffff-no-rj',
+  },
+  {
+    user: 'sugashaw',
+    image:
+      'https://yt3.ggpht.com/ytc/AKedOLRY9Un_v7Xr9dG1F5NEkqGsGSqwqRz0O3w3r1mI=s900-c-k-c0x00ffffff-no-rj',
+  },
+  {
+    user: 'NazDumanskyy',
+    image:
+      'https://yt3.ggpht.com/ytc/AKedOLRY9Un_v7Xr9dG1F5NEkqGsGSqwqRz0O3w3r1mI=s900-c-k-c0x00ffffff-no-rj',
+  },
+  {
+    user: 'aplasticplant',
+    image:
+      'https://yt3.ggpht.com/ytc/AKedOLRY9Un_v7Xr9dG1F5NEkqGsGSqwqRz0O3w3r1mI=s900-c-k-c0x00ffffff-no-rj',
+  },
+];
